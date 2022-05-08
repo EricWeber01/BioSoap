8a5c8de66582c6c32a24b4efa0099d3d5abc2ad8
babya <babya@ERIC_WEBER>
2022-05-08 20:01:26 +0300

    Add project files.

---
+++ BioSoap/Data.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioSoap
+{
+        [Serializable]
+        class Location
+        {
+            public string Adress { get; set; }
+            public int StudCount { get; set; }
+            public override string ToString()
+            {
+                return $"Адресс: {Adress} | Вместительность: {StudCount} студ.";
+            }
+
+        }
+        [Serializable]
+        class Subject
+        {
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Name}";
+            }
+        }
+        [Serializable]
+        class Student
+        {
+            string FIO { get; set; }
+            string School { get; set; }
+            public Subject[] Subjects;
+            Location Loc;
+            public Student(string fio, string school, Location loc, List<Subject> subjects)
+            {
+                FIO = fio;
+                School = school;
+                Loc = loc;
+                Subjects = subjects.ToArray();
+            }
+            public override string ToString()
+            {
+                string str = "Предметы: ";
+                foreach (var item in Subjects)
+                    str += item.ToString() + " | ";
+                return $"{FIO} | {School} \n{Loc}\n{str}";
+            }
+        }
+        [Serializable]
+        class Data
+    {
+            List<Student> Students;
+            public Data()
+            {
+                Students = new List<Student>();
+            }
+            public void AddStud(Student s)
+            {
+                Students.Add(s);
+            }
+            public void AddToBinary()
+            {
+                BinaryFormatter binary = new BinaryFormatter();
+                using (FileStream fs = new FileStream("stud.bin", FileMode.OpenOrCreate))
+                {
+                    binary.Serialize(fs, Students);
+                }
+            }
+            public void GetFromBinary()
+            {
+                BinaryFormatter binary = new BinaryFormatter();
+                using (FileStream fs = new FileStream("stud.bin", FileMode.OpenOrCreate))
+                {
+                    Students = (List<Student>)binary.Deserialize(fs);
+
+                }
+            }
+
+            public void SoapSave()
+            {
+                SoapFormatter formatter = new SoapFormatter();
+                using (FileStream fs = new FileStream("save.soap", FileMode.OpenOrCreate))
+                {
+                    Student[] std = Students.ToArray();
+
+                    formatter.Serialize(fs, std);
+                }
+            }
+            public void GetFromSoap()
+            {
+                SoapFormatter soap = new SoapFormatter();
+                using (FileStream fs = new FileStream("save.soap", FileMode.OpenOrCreate))
+                {
+                    Student[] std = (Student[])soap.Deserialize(fs);
+
+                    Students = std.ToList();
+
+                }
+            }
+            public void Show()
+            {
+                foreach (var item in Students)
+                    Console.WriteLine(item + "\n");
+            }
+        }
+}
---
+++ BioSoap/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioSoap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Data dataBase = new Data();
+
+            List<Subject> fizmat = new List<Subject> { new Subject() { Name = "Английский"},
+                new Subject() { Name = "Украинский язык" },
+                new Subject() { Name = "Математика"}};
+            List<Subject> gum = new List<Subject> { new Subject() { Name = "История Украины"},
+                new Subject() { Name = "Украинский язык и литература" },
+                new Subject() { Name = "География"}};
+            dataBase.AddStud(new Student("Зубенко Михаил", "КЗШ №21", new Location() { Adress = "пр.Мира 10", StudCount = 300 }, fizmat));
+            dataBase.AddStud(new Student("Жмышенко Валерий", "КЗШ №22", new Location() { Adress = "пр.Мира 10", StudCount = 300 }, fizmat));
+            dataBase.AddStud(new Student("Богдан Богомдан", "КЗШ №23", new Location() { Adress = "пр.Мира 10", StudCount = 300 }, gum));
+            dataBase.AddStud(new Student("Сухачёв Гарик", "КЗШ №24", new Location() { Adress = "пр.Мира 10", StudCount = 300 }, gum));
+            //dataBase.AddToBinary();
+            dataBase.SoapSave();
+            /*Console.WriteLine("GET FROM BINARY");
+            ZnoDataBase dataBase2 = new ZnoDataBase();
+            dataBase2.GetFromBinary();*/
+
+            //dataBase2.Show();
+
+            Console.WriteLine("GET FROM SOAP");
+            Data dataBase3 = new Data();
+            dataBase3.GetFromSoap();
+            dataBase3.Show();
+        }
+    }
+}
